98a500f2d389591018a3a11d9a74604cd3e5437f
ralf <rb@egroupware.org>
2026-04-22 11:04:46 +0200

    add app-toolbar

---
+++ js/app.ts
@@ -0,0 +1,33 @@
+/**
+ * EGroupware OpenIDConnect
+ *
+ * @package openid
+ * @link https://www.egroupware.org
+ * @author Ralf Becker <rb@egroupware.org>
+ * @license https://opensource.org/licenses/gpl-license.php GPL - GNU General Public License
+ */
+
+import {EgwApp} from '../../api/js/jsapi/egw_app';
+import {app} from "../../api/js/jsapi/egw_global";
+import type {etemplate2} from "../../api/js/etemplate/etemplate2";
+
+/**
+ * UI for OpenIDConnect
+ */
+export class OpenIDConnectApp extends EgwApp
+{
+	et2_ready(et2: etemplate2, name: string)
+	{
+		super.et2_ready(et2, name);
+
+		switch (name)
+		{
+			case 'openid.clients':
+				app.admin?.enableAppToolbar(et2, name);
+				break;
+		}
+	}
+}
+
+// Register the app with EGroupware
+app.classes.openid = OpenIDConnectApp;
